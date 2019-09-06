03fcc88722f2f2aa560707545bfc1de844e88786
Mike Harder <mharder@microsoft.com>
2019-09-06 13:06:59 -0700

    Add NODE_TLS_REJECT_UNAUTHORIZED

--- js/run.sh
+++ js/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --network host -e STORAGE_CONNECTION_STRING storagestress/js "$@"
+docker run -it --rm --network host -e STORAGE_CONNECTION_STRING -e NODE_TLS_REJECT_UNAUTHORIZED storagestress/js "$@"
